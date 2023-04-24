d07ba3f409598bd012d08794615899c6669f13fc
Troy Dieter <troydieter@gmail.com>
2023-04-24 12:00:23 -0400

    Removed back-end config

--- aws-terraform-hybrid-dns/global/iam/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "jennas-tf-statefiles-master"
-    key     = "global/iam/terraform.tfstate"
-    region  = "us-east-1"
-    profile = "default"
-    encrypt = true
-  }
-}
